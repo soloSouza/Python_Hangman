583e877f68f09cf5f807cea6b6b1eb5bb588b48e
soloSouza <134002966+soloSouza@users.noreply.github.com>
2023-05-23 23:54:12 -0300

    Included logic for random word selection from the word list

--- script.py
+++ script.py
@@ -1,7 +1,11 @@
 import random
 
+# Import word list from wordList.txt and store as word_list in the program:
 word_list = []
 with open("wordList.txt") as wordList_txt:
     for word in wordList_txt:
         word_list.append(word)
 
+# Random word selection for the round:
+random_word_index = random.randint(1,len(word_list)+1)
+random_word = word_list[random_word_index]
