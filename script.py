e0d253371937e8c386410f6f4f6d112a8d7533f9
soloSouza <134002966+soloSouza@users.noreply.github.com>
2023-05-24 12:40:38 -0300

    updated visual aids for the games and started working on the gameplay

--- script.py
+++ script.py
@@ -1,3 +1,243 @@
+
+game_header = """ 
+------------------------------------------------------------------------------------------------
+|XX|--------------------s
+|XX|                    s
+|XX|                   sss 
+|XX|                   sss
+|XX|                  s   s 
+|XX|                 s     s
+|XX|                  s s s
+|XX|
+|XX|  WELCOME TO:
+|XX|
+|XX|  TTTTTTT  H     H  EEEEEEE    H     H     A     N     N   GGGGG   M     M     A     N     N
+|XX|     T     H     H  E          H     H    A A    NN    N  G     G  MM   MM    A A    NN    N
+|XX|     T     H     H  E          H     H   A   A   N N   N  G        M M M M   A   A   N N   N
+|XX|     T     HHHHHHH  EEEEE      HHHHHHH  AAAAAAA  N  N  N  G  GGGG  M  M  M  AAAAAAA  N  N  N 
+|XX|     T     H     H  E          H     H  A     A  N   N N  G  G  G  M     M  A     A  N   N N
+|XX|     T     H     H  E          H     H  A     A  N    NN  G     G  M     M  A     A  N    NN
+|XX|     T     H     H  EEEEEEE    H     H  A     A  N     N   GGGGG   M     M  A     A  N     N
+|XX|   
+------------------------------------------------------------------------------------------------   
+"""
+hangman_initial = """
+|XX|--------------------s
+|XX|                    s
+|XX|                    s
+|XX|                    s
+|XX|                    s
+|XX|                    s
+|XX|                    s
+|XX|                   sss
+|XX|                   sss
+|XX|                  s   s
+|XX|                 s     s
+|XX|                  s s s
+|XX|                    
+|XX|               
+|XX|               
+|XX|               
+|XX|                  
+|XX|                  
+|XX|                  
+|XX|                 
+|XX|                
+|XX|
+|XX|
+|XX|
+---------------------------------------
+"""
+hangman_one_miss = """
+|XX|--------------------s
+|XX|                    s
+|XX|                    s
+|XX|                    s
+|XX|                    s
+|XX|                   ooo
+|XX|                 o     o
+|XX|                o  O O  o
+|XX|                o  ---  o
+|XX|                 o     o
+|XX|                   sss
+|XX|            
+|XX|            
+|XX|            
+|XX|            
+|XX|            
+|XX|            
+|XX|            
+|XX|            
+|XX|            
+|XX|            
+|XX|
+|XX|
+|XX|
+---------------------------------------
+"""
+hangman_two_misses = """
+|XX|--------------------s
+|XX|                    s
+|XX|                    s
+|XX|                    s
+|XX|                    s
+|XX|                   ooo
+|XX|                 o     o
+|XX|                o  O O  o
+|XX|                o  ---  o
+|XX|                 o     o
+|XX|                   sss
+|XX|                   |z|
+|XX|                   |z|
+|XX|                   |z|
+|XX|                   |z|
+|XX|                   |z|
+|XX|                   |z|
+|XX|                   |z|
+|XX|            
+|XX|            
+|XX|            
+|XX|
+|XX|
+|XX|
+---------------------------------------
+"""
+hangman_three_misses = """
+
+|XX|--------------------s
+|XX|                    s
+|XX|                    s
+|XX|                    s
+|XX|                    s
+|XX|                   ooo
+|XX|                 o     o
+|XX|                o  O O  o
+|XX|                o  ---  o
+|XX|                 o     o
+|XX|                   sss
+|XX|                   |z|
+|XX|                  /|z|
+|XX|                 //|z|
+|XX|                // |z|
+|XX|               //  |z|
+|XX|              '''  |z|
+|XX|                   |z|
+|XX|               
+|XX|               
+|XX|               
+|XX|
+|XX|
+|XX|
+---------------------------------------
+"""
+hangman_four_misses ="""
+|XX|--------------------s
+|XX|                    s
+|XX|                    s
+|XX|                    s
+|XX|                    s
+|XX|                   ooo
+|XX|                 o     o
+|XX|                o  O O  o
+|XX|                o  ---  o
+|XX|                 o     o
+|XX|                   sss
+|XX|                   |z|
+|XX|                  /|z|
+|XX|                 //|z|
+|XX|                // |z|
+|XX|               //  |z|
+|XX|              '''  |z|
+|XX|                   |z|
+|XX|                     \\\\
+|XX|                      \\\\
+|XX|                       \\\\
+|XX|                       ''''
+|XX|
+|XX|
+---------------------------------------
+"""
+hangman_five_misses = """
+|XX|--------------------s
+|XX|                    s
+|XX|                    s
+|XX|                    s
+|XX|                    s
+|XX|                   ooo
+|XX|                 o     o
+|XX|                o  O O  o
+|XX|                o  ---  o
+|XX|                 o     o
+|XX|                   sss
+|XX|                   |z|
+|XX|                  /|z|\\
+|XX|                 //|z|\\\\
+|XX|                // |z| \\\\
+|XX|               //  |z|  \\\\
+|XX|              '''  |z|  '''
+|XX|                   |z|
+|XX|                     \\\\
+|XX|                      \\\\
+|XX|                       \\\\
+|XX|                       ''''
+|XX|
+|XX|
+---------------------------------------
+"""
+hangman_six_misses = """
+|XX|--------------------s
+|XX|                    s
+|XX|                    s
+|XX|                    s
+|XX|                    s
+|XX|                   ooo
+|XX|                 o     o
+|XX|                o  o o  o
+|XX|                o  ---  o
+|XX|                 o     o
+|XX|                   sss
+|XX|                   |z|
+|XX|                  /|z|\\
+|XX|                 //|z|\\\\
+|XX|                // |z| \\\\
+|XX|               //  |z|  \\\\
+|XX|              '''  |z|  '''
+|XX|                   |z|
+|XX|                  // \\\\
+|XX|                 //   \\\\
+|XX|                //     \\\\
+|XX|              ''''     ''''
+|XX|
+|XX|
+---------------------------------------
+"""
+hangman_game_over = """
+|XX|--------------------s
+|XX|                    s
+|XX|                    s
+|XX|                    s
+|XX|                    s
+|XX|                   ooo
+|XX|                 o     o
+|XX|                o  x x  o
+|XX|                o  ...  o
+|XX|                 o     o
+|XX|                   sss
+|XX|                   |z|
+|XX|                  /|z|\\
+|XX|                 //|z|\\\\
+|XX|                // |z| \\\\
+|XX|               //  |z|  \\\\
+|XX|              '''  |z|  '''
+|XX|                   |z|
+|XX|                  // \\\\
+|XX|                 //   \\\\
+|XX|                //     \\\\
+|XX|              ''''     ''''
+|XX|
+|XX|
+---------------------------------------
+"""
 import random
 
 # Import word list from wordList.txt and store as word_list in the program:
@@ -7,224 +247,9 @@ with open("wordList.txt") as wordList_txt:
         word_list.append(word)
 
 # Random word selection for the round:
-random_word_index = random.randint(1,len(word_list)+1)
+random_word_index = random.randint(1,len(word_list))
 random_word = word_list[random_word_index]
 
-# hangman initial
-print(
-"""
-
-|--|--------------------s
-|  |                    s
-|  |                    s
-|  |                    s
-|  |                    s
-|  |                    s
-|  |                    s
-|  |                   sss
-|  |                   sss
-|  |                  s   s
-|  |                 s     s
-|  |                  s s s
-|  |                    
-|  |               
-|  |               
-|  |               
-|  |                  
-|  |                  
-|  |                  
-|  |                 
-|  |                
-|  |
-|  |
-|  |
----------------------------------------
-""")
-
-# hangman first-miss:
-
-print(
-"""
-
-|--|--------------------s
-|  |                    s
-|  |                    s
-|  |                    s
-|  |                    s
-|  |                   ooo
-|  |                 o     o
-|  |                o  O O  o
-|  |                o  ---  o
-|  |                 o     o
-|  |                   sss
-|  |            
-|  |            
-|  |            
-|  |            
-|  |            
-|  |            
-|  |            
-|  |            
-|  |            
-|  |            
-|  |
-|  |
-|  |
----------------------------------------
-""")
-
-# hangman second-miss:
-
-print(
-"""
-
-|--|--------------------s
-|  |                    s
-|  |                    s
-|  |                    s
-|  |                    s
-|  |                   ooo
-|  |                 o     o
-|  |                o  O O  o
-|  |                o  ---  o
-|  |                 o     o
-|  |                   sss
-|  |                   | |
-|  |                   | |
-|  |                   | |
-|  |                   | |
-|  |                   | |
-|  |                   | |
-|  |                   | |
-|  |            
-|  |            
-|  |            
-|  |
-|  |
-|  |
----------------------------------------
-""")
-
-# hangman third-miss:
-print(
-"""
-
-|--|--------------------s
-|  |                    s
-|  |                    s
-|  |                    s
-|  |                    s
-|  |                   ooo
-|  |                 o     o
-|  |                o  O O  o
-|  |                o  ---  o
-|  |                 o     o
-|  |                   sss
-|  |                   | |
-|  |                  /| |
-|  |                 //| |
-|  |                // | |
-|  |               //  | |
-|  |                   | |
-|  |                   | |
-|  |               
-|  |               
-|  |               
-|  |
-|  |
-|  |
----------------------------------------
-""")
-
-# hangman fourth-miss:
-print(
-"""
-
-|--|--------------------s
-|  |                    s
-|  |                    s
-|  |                    s
-|  |                    s
-|  |                   ooo
-|  |                 o     o
-|  |                o  O O  o
-|  |                o  ---  o
-|  |                 o     o
-|  |                   sss
-|  |                   | |
-|  |                  /| |
-|  |                 //| |
-|  |                // | |
-|  |               //  | |
-|  |                   | |
-|  |                   | |
-|  |                     \\\\
-|  |                      \\\\
-|  |                       \\\\
-|  |
-|  |
-|  |
----------------------------------------
-""")
-
-# hangman fifth-miss:
-print(
-"""
-
-|--|--------------------s
-|  |                    s
-|  |                    s
-|  |                    s
-|  |                    s
-|  |                   ooo
-|  |                 o     o
-|  |                o  O O  o
-|  |                o  ---  o
-|  |                 o     o
-|  |                   sss
-|  |                   | |
-|  |                  /| |\\
-|  |                 //| |\\\\
-|  |                // | | \\\\
-|  |               //  | |  \\\\
-|  |                   | |
-|  |                   | |
-|  |                     \\\\
-|  |                      \\\\
-|  |                       \\\\
-|  |
-|  |
-|  |
----------------------------------------
-""")
-
-# hangman game-over:
-print(
-"""
-
-|--|--------------------s
-|  |                    s
-|  |                    s
-|  |                    s
-|  |                    s
-|  |                   ooo
-|  |                 o     o
-|  |                o  x x  o
-|  |                o  ...  o
-|  |                 o     o
-|  |                   sss
-|  |                   | |
-|  |                  /| |\\
-|  |                 //| |\\\\
-|  |                // | | \\\\
-|  |               //  | |  \\\\
-|  |                   | |
-|  |                   | |
-|  |                  // \\\\
-|  |                 //   \\\\
-|  |                //     \\\\
-|  |
-|  |
-|  |
----------------------------------------
-""")
+print(game_header)
+print("\"THE HANGMAN\" is a single player terminal game.\n")
+start_game = input("Would you like to start a new game? (y/n): ")
